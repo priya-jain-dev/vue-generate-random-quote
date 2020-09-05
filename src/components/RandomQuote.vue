b38ba0859bfa9f5db3bb6c2058a27bba7b198b20
Priya Jain <priya.jain@dingg.app>
2020-09-05 21:08:35 +0530

    ✨author and genre show

--- src/components/RandomQuote.vue
+++ src/components/RandomQuote.vue
@@ -1,18 +1,23 @@
 <template>
   <v-main>
     <v-container>
-      <v-row align="end" justify="end" class="top-right">
-        <p class="random">RANDOM<v-icon style="color: black;">{{ icons.mdiAutorenew }}</v-icon>
+      <div class="top-right" v-on:click="generateQuote">
+        <p class="random">RANDOM<v-icon style="color: black;"  :class="{_spin:is_spin}">{{ icons.mdiAutorenew }}</v-icon>
         </p>
-      </v-row>
-      <v-row align="center" justify="center">
+      </div>
+      <v-row align="center" justify="center" v-if="quote">
         <v-col cols="12" sm="8" md="6">
           <img alt="Random logo" src="../assets/thinking.svg" width="250" />
           <v-row align="center" justify="center">
             <v-col cols="11" class="v-line">
               <p class="quote">{{ quote.data.quote.quoteText }}</p>
             </v-col>
-          </v-row>
+            <v-col class="align-self-start text-left ml-15">
+                <p class="author">{{quote.data.quote.quoteAuthor}}</p>
+                <p class="type">{{quote.data.quote.quoteGenre}}</p>
+            </v-col>
+          </v-row >
+         
         </v-col>
       </v-row>
     </v-container>
@@ -33,35 +38,28 @@ export default {
       mdiAutorenew,
       mdiArrowRight
     },
-    quote: null
+    quote: null,
+    is_spin:false
   }),
   mounted() {
-    axios
+    this.generateQuote();
+  },
+  methods: {
+    generateQuote: function () {
+     this.is_spin = true
+     axios
       .get("https://quote-garden.herokuapp.com/api/v2/quotes/random")
       .then(response => {
         this.quote = response;
-        console.log(response);
+        this.is_spin = false;
       });
+    }
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 .container {
   display: flex;
   flex-wrap: wrap;
@@ -86,5 +84,30 @@ margin-left: 30px;
 
 .v-line {
   border-left: 8px solid #f7df94;
+}
+._spin{
+    animation: spin-animation 0.5s infinite;
+    display: inline-block;
+}
+@keyframes spin-animation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(359deg);
+  }
+}
+.author{
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 20px;
+    color: #4F4F4F;
+}
+.type{
+    font-weight: 500;
+font-size: 14px;
+line-height: 16px;
+color: #828282;
+
 }
 </style>
